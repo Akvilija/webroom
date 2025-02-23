99be42d7f35f1c4c8495098fa36683f6ae545d1a
Akvilija <akvilija.skorupskaite199503@gmail.com>
2025-02-23 11:08:05 +0200

    added products

--- shop-project/src/App.tsx
+++ shop-project/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Layout from './components/Layout'
+import ProductsPage from './pages/ProductsPage/ProductsPage'
 
 function App(): JSX.Element {
 
 
   return (
     <>
-      <Header />
+      <Layout header={<Header />} >
+        <ProductsPage />
+      </Layout>
     </>
   )
 }
